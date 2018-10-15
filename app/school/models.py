2410cbe96d2ef9042e719d88361189bad5065ef8
hanyonghee <hanyonghee9264@gmail.com>
2018-10-15 14:03:15 +0900

    STOP-OUT시험 제출

---
+++ app/school/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class School(models.Model):
+    pass
+
+
+class Student(models.Model):
+    pass
---
+++ app/school/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+class School:
+    def __init__(self, name):
+        self.name = name
+
+
+class Student():
+    def __init__(self):
+
